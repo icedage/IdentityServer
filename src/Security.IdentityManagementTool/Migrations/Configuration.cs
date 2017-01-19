9f1f0ce4180fa3ea100719546ea5059387880422
Veronica Zotali <veronica.zotali@just-eat.com>
2017-01-19 22:09:23 +0000

    merge

--- src/Security.IdentityManagementTool/Migrations/Configuration.cs
+++ src/Security.IdentityManagementTool/Migrations/Configuration.cs
@@ -1,4 +1,9 @@
+using System;
 using System.Data.Entity.Migrations;
+using System.Linq;
+using Security.IdentityManagementTool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Security.IdentityManagementToolMigrations
 {
@@ -11,18 +16,31 @@ namespace Security.IdentityManagementToolMigrations
 
         protected override void Seed(Security.IdentityManagementTool.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "SuperPowerUser",
+                Email = "Cary.Grant@gmail.com",
+                EmailConfirmed = true,
+                FirstName = "Cary",
+                LastName = "Grant",
+                BirthDate = DateTime.Now
+            };
+
+            manager.Create(user, "P@ssword123");
+
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+
+            var adminUser = manager.FindByName("SuperPowerUser");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin" });
         }
     }
 }
